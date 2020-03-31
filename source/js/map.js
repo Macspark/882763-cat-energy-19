3ef6fb674f8ac7c6512cb3fd1930d10b98d4ed1d
macspark <57904588+Macspark@users.noreply.github.com>
2020-03-31 15:47:41 +0400

    Поправил картинки

--- source/js/map.js
+++ source/js/map.js
@@ -7,9 +7,9 @@ var map = new ymaps.Map("map", {
 
 var mapPlacemark = new ymaps.Placemark([59.938631, 30.323155], {}, {
   iconLayout: 'default#image',
-  iconImageHref: 'img/map-pointer.png',
+  iconImageHref: 'img/map-pin.png',
   iconImageSize: [55, 53],
-  iconImageOffset: [-22, -53]
+  iconImageOffset: [-27, -53]
 });
 
 map.geoObjects.add(mapPlacemark);
