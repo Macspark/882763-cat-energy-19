b7a97022b415ca5f73fe01649f3f029fdf72a1e8
macspark <57904588+Macspark@users.noreply.github.com>
2020-04-10 12:34:47 +0400

    Борис: последние штрихи?

--- source/js/menu.js
+++ source/js/menu.js
@@ -1,12 +1,13 @@
 var navMain = document.querySelector('.main-nav');
-var navToggle = document.querySelector('.main-nav__toggle');
+var navBtn = document.querySelector('.main-nav__toggle');
+var navToggled = 'main-nav--toggled';
 
 navMain.classList.remove('main-nav--nojs');
 
-navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--toggled')) {
-    navMain.classList.remove('main-nav--toggled');
+navBtn.addEventListener('click', function() {
+  if (navMain.classList.contains(navToggled)) {
+    navMain.classList.remove(navToggled);
   } else {
-    navMain.classList.add('main-nav--toggled');
+    navMain.classList.add(navToggled);
   }
 });
